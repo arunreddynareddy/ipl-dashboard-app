6523b44d0b5a0bb8d555decd6c6286c80b0e075d
arunreddynareddy <arunreddynareddy@gmail.com>
2024-06-18 12:27:22 +0000

    add back button and pie chart

--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -1,5 +1,6 @@
 import {Component} from 'react'
 import Loader from 'react-loader-spinner'
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
 
 import LatestMatch from '../LatestMatch'
 import MatchCard from '../MatchCard'
@@ -52,6 +53,7 @@ class TeamMatches extends Component {
   renderRecentMatchesList = () => {
     const {teamMatchesData} = this.state
     const {recentMatches} = teamMatchesData
+    console.log(recentMatches)
 
     return (
       <ul className="recent-matches-list">
@@ -59,6 +61,68 @@ class TeamMatches extends Component {
           <MatchCard matchDetails={recentMatch} key={recentMatch.id} />
         ))}
       </ul>
+    )
+  }
+
+  onClickNavigateHome = () => {
+    const {history} = this.props
+    history.push('/')
+  }
+
+  renderPieChartView = () => {
+    const {teamMatchesData} = this.state
+    const {recentMatches} = teamMatchesData
+
+    const wonMatches = recentMatches.filter(each => each.matchStatus === 'Won')
+    const lostMatches = recentMatches.filter(
+      each => each.matchStatus === 'Lost',
+    )
+    const drawnMatches = recentMatches.filter(
+      each => each.matchStatus === 'Drawn',
+    )
+    const pieData = [
+      {
+        count: wonMatches.length,
+        status: 'Won',
+      },
+      {
+        count: lostMatches.length,
+        status: 'Lost',
+      },
+      {
+        count: drawnMatches.length,
+        status: 'Drawn',
+      },
+    ]
+
+    return (
+      <div>
+        <h1 className="pie-chart-heading">Match Results Chart</h1>
+        <ResponsiveContainer width="60%" height={300}>
+          <PieChart>
+            <Pie
+              cx="60%"
+              cy="40%"
+              data={pieData}
+              startAngle={0}
+              endAngle={360}
+              innerRadius="40%"
+              outerRadius="70%"
+              dataKey="count"
+            >
+              <Cell name="Won" fill="#18ed66" />
+              <Cell name="Lost" fill="#e31a1a" />
+              <Cell name="Drawn" fill="#f7db00" />
+            </Pie>
+            <Legend
+              iconType="circle"
+              layout="vertical"
+              verticalAlign="middle"
+              align="right"
+            />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
     )
   }
 
@@ -71,12 +135,20 @@ class TeamMatches extends Component {
         <img src={teamBannerURL} alt="team banner" className="team-banner" />
         <LatestMatch latestMatchData={latestMatch} />
         {this.renderRecentMatchesList()}
+        {this.renderPieChartView()}
+        <button
+          type="button"
+          className="back-button"
+          onClick={this.onClickNavigateHome}
+        >
+          Back
+        </button>
       </div>
     )
   }
 
   renderLoader = () => (
-    <div data-testid="loader" className="loader-container">
+    <div testid="loader" className="loader-container">
       <Loader type="Oval" color="#ffffff" height={50} />
     </div>
   )
